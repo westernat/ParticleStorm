dbdfe9aed245534a2a1b2b98746483c7156e6ff8
westernat <westernat233@163.com>
2026-06-17 09:59:14 +0800

    codec

--- src/main/java/org/mesdag/particlestorm/data/event/SoundEffect.java
+++ src/main/java/org/mesdag/particlestorm/data/event/SoundEffect.java
@@ -1,7 +1,6 @@
 package org.mesdag.particlestorm.data.event;
 
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
@@ -20,7 +19,7 @@ public record SoundEffect(Holder<SoundEvent> soundEffect) implements IEventNode 
     public static final Codec<Holder<SoundEvent>> SOUND_EFFECT_CODEC = RegistryFileCodec.create(Registries.SOUND_EVENT, RecordCodecBuilder.create(instance -> instance.group(
             ResourceLocation.CODEC.fieldOf("event_name").forGetter(SoundEvent::getLocation)
     ).apply(instance, SoundEvent::createVariableRangeEvent)));
-    public static final MapCodec<SoundEffect> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
+    public static final Codec<SoundEffect> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             SOUND_EFFECT_CODEC.fieldOf("sound_effect").orElseGet(() -> Holder.direct(SoundEvents.EMPTY)).forGetter(SoundEffect::soundEffect)
     ).apply(instance, SoundEffect::new));
 
